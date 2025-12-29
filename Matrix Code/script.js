2bba4aa5ff1b3137a2f492382ee823e72264c35e
akshatjaiswal-2003 <akshat.work108@gmail.com>
2025-12-29 23:00:13 +0530

    first

--- Matrix Code/script.js
+++ Matrix Code/script.js
@@ -1,39 +1,26 @@
-// var para = document.querySelector('p')
-// var charecters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-// var text = para.innerText
-// para.addEventListener('mouseenter', function () {
-//     setInterval(function () {
-//         var str = text.split('').map((char, index) => {
-//             return charecters.split('')[Math.floor(Math.random() * 53)]
-//         }).join('')
-//         para.innerText = str
-//     }, 30)
-// })
+var para = document.querySelector('p')
+var text = para.innerText
+var charecters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+var iteration = 0
+var interval = null
 
+function randomText(){
+    var str = text.split('').map(function(char,index){
+        if(iteration > index) return char
+        return charecters.split('')[Math.floor(Math.random()*52)]
+    }).join('')
+    para.innerText = str
+    iteration += 0.3
 
-// var para = document.querySelector('p');
-// var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-// var originalText = para.innerText;
+    if(iteration > text.length){
+        clearInterval(interval)
+        interval = null
+    }
+}
 
-// para.addEventListener('mouseenter', function () {
-//     let i = 0;
-
-//     var interval = setInterval(function () {
-//         para.innerText = originalText
-//             .split("")
-//             .map((char, index) => {
-//                 if (index < i) {
-//                     return originalText[index];
-//                 }
-//                 return characters[Math.floor(Math.random() * characters.length)];
-//             })
-//             .join("");
-
-//         if (i >= originalText.length) {
-//             clearInterval(interval);
-//         }
-
-//         i++;
-//     }, 40);
-// });
+para.addEventListener('mouseenter', ()=>{
+    iteration = 0
+    clearInterval(interval)
+    interval = setInterval(randomText,30)
+})
 
